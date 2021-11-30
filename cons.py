52f11c2e1793408004d0bf4b3339aead96d29692
Holden Fox <79114351+climbtrad@users.noreply.github.com>
2021-11-30 15:38:45 -0700

    Update cons.py

--- cons.py
+++ cons.py
@@ -1,10 +1,10 @@
 # Parse fasta into matrix
-
 def parse_fasta(data):
     list = []
     strings = data.strip().split('>')
 
     for data in strings:
+
         if len(data) == 0:
             continue
 
@@ -14,15 +14,19 @@ def parse_fasta(data):
         list.append(bases)
 
     matrix = []
+
     for i in range(len(list)):
         matrix.append([*list[i]])
 
     return matrix
 
+
 def profile(matrix):
     profile = []
+
     for i in range(len(matrix)):
         a, c, t, g = 0
+
         for j in range(len(matrix)):
             if matrix[j:i] == 'A':
                 a += 1
@@ -33,7 +37,9 @@ def profile(matrix):
             if matrix[j:i] == 'G':
                 g += 1
         profile.append(a, c, t, g)
+
     return profile
+
 
 def consensus(matrix):
     consensus_seq = ""
@@ -41,7 +47,6 @@ def consensus(matrix):
 
 
 if __name__ == '__main__':
-
     data = open('datasets/test.fasta').read()
     matrix = (parse_fasta(data))
     print(profile(matrix))
